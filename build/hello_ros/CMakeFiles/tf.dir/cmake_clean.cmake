96c36667eb506e438d3239b246527802ab54b4d4
yin <yinzhe23@bupt.edu.cn>
2024-10-07 00:18:54 +0800

    任务3

---
+++ build/hello_ros/CMakeFiles/tf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tf.dir/src/tf2_node.cpp.o"
+  "CMakeFiles/tf.dir/src/tf2_node.cpp.o.d"
+  "tf"
+  "tf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/tf2_node.cpp
+++ src/tf2_node.cpp
@@ -3,20 +3,26 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/pose.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include <functional>
+#include <rclcpp/subscription.hpp>
+#include <string>
+#include <turtlesim/msg/detail/pose__struct.hpp>
+#include <vector>
 
 class node: public rclcpp::Node
 {
 private:
     tf2_ros::TransformBroadcaster tfb;
-    std::string turtleName;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
+    // std::string turtleName;
+    // rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
+    std::vector<rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr> subscriptions_;
 
-    void pose_callback(const turtlesim::msg::Pose::SharedPtr msg)
+    void pose_callback(const turtlesim::msg::Pose::SharedPtr msg, const std::string turtle_name)
     {
         geometry_msgs::msg::TransformStamped t;
         t.header.stamp = this->get_clock()->now();
         t.header.frame_id = "world";
-        t.child_frame_id = turtleName;
+        t.child_frame_id = turtle_name;
         t.transform.translation.x = msg->x;
         t.transform.translation.y = msg->y;
         t.transform.translation.z = 0.0;
@@ -29,10 +35,14 @@ private:
         tfb.sendTransform(t);
     }
 public:
-    node(std::string st): Node(st + "_boardcaster"), tfb(this), turtleName(st)
+    node(std::vector<std::string> turtle_names): Node("turtles_boardcaster"), tfb(this)
     {
-        subscription_ = this->create_subscription<turtlesim::msg::Pose>(
-            turtleName+"/pose", 10, std::bind(&node::pose_callback, this, std::placeholders::_1));
+        for (const auto & turtle_name : turtle_names){
+            subscriptions_.push_back(this->create_subscription<turtlesim::msg::Pose>(
+            turtle_name+"/pose", 10, [this, turtle_name](const turtlesim::msg::Pose::SharedPtr msg) {this->pose_callback(msg, turtle_name);}));
+            // turtle_name+"/pose", 10, std::bind(&node::pose_callback, this, std::placeholders::_1, turtle_name)));
+        }
+        
         RCLCPP_INFO(this->get_logger(), "Hello, world");
     }
     ~node()
@@ -44,10 +54,14 @@ public:
 class ROS_EVENT_LOOP
 {
 public:
-    ROS_EVENT_LOOP(int argc, char *argv[],std::string st)
+    ROS_EVENT_LOOP(int argc, char *argv[])
     {
         rclcpp::init(argc, argv);
-        rclcpp::spin(std::make_shared<node>(st));
+        std::vector<std::string> turtle_names;
+        for (int i = 1; i < argc; i++){
+            turtle_names.push_back(argv[i]);
+        }
+        rclcpp::spin(std::make_shared<node>(turtle_names));
     }
     ~ROS_EVENT_LOOP()
     {
@@ -57,11 +71,11 @@ public:
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2 )
+    if (argc < 2 )
     {
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: tf turtle_name");
         return 1;
     }
-    ROS_EVENT_LOOP(argc, argv, argv[1]);
+    ROS_EVENT_LOOP(argc, argv);
     return 0;
 }
